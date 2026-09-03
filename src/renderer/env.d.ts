306b581f41977b90969b641d263f64bbc6deda97
zshnrdtya <radityaraizeeshan@gmail.com>
2026-09-03 23:47:10 +0700

    types: clean env.d.ts from electron window declarations

--- src/renderer/env.d.ts
+++ src/renderer/env.d.ts
@@ -1,13 +1,5 @@
 /// <reference types="vite/client" />
 
-import { ElectronAPI } from '../preload/index'
-
-declare global {
-  interface Window {
-    electronAPI: ElectronAPI
-  }
-}
-
 declare module '*.vrm' {
   const content: string
   export default content
